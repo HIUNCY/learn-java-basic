8b0a9e3c13ec743de52e5fd3f7c22e9145c01d7a
HIUNCY <zainulkamal393@gmail.com>
2024-09-19 13:58:46 +0700

    Tipe Data Char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,10 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char z = 'Z';
+        char a = 'A';
+        char y = 'Y';
+        System.err.print(z);
+        System.err.print(a);
+        System.err.print(y);
+    }
+}
