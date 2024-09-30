284c40c727f9d7786f8d6c75756cd1bbfdd7811f
HIUNCY <wolesajay@gmail.com>
2024-09-30 13:23:11 +0700

    Perulangan

---
+++ src/Perulangan.java
@@ -0,0 +1,23 @@
+public class Perulangan {
+    public static void main(String[] args) {
+        // FOR LOOP
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Perulangan For ke-"+i);
+        }
+        
+        // WHILE LOOP
+        int j = 1;
+        while (j <= 10) {
+            System.out.println("Perulangan While ke-"+j);
+            j++;
+        }
+        
+        // DO WHILE LOOP
+        int k = 1;
+        do {
+            System.out.println("Perulangan Do While ke-"+k);
+            k++;
+        } while (k <= 10);
+
+    }
+}
