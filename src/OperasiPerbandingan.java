da7ad8f92056c702e416c32b7b9eab650d3593ad
HIUNCY <wolesajay@gmail.com>
2024-09-30 10:57:18 +0700

    Operasi Perbandingan

---
+++ src/OperasiPerbandingan.java
@@ -0,0 +1,12 @@
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        int val1 = 100;
+        int val2 = 100;
+        System.out.println(val1>val2);
+        System.out.println(val1>=val2);
+        System.out.println(val1<val2);
+        System.out.println(val1<=val2);
+        System.out.println(val1==val2);
+        System.out.println(val1!=val2);
+    }
+}
