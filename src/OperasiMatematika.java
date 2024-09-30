d83a7cd24ac90170e4917e07dd081d7d97a435a8
HIUNCY <wolesajay@gmail.com>
2024-09-30 10:54:16 +0700

    Operasi Matematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,23 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 21;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        a += 10;
+        b -= 6;
+        System.out.println(a);
+        System.out.println(b);
+        
+        a++;
+        b--;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
